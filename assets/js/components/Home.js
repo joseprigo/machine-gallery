9e7cd540d0c92d201318f7976b27319cfc0f0b6f
josep rigo <jrrr13@gmail.com>
2020-11-15 23:06:57 +0100

    table layout colors

--- assets/js/components/Home.js
+++ assets/js/components/Home.js
@@ -1,18 +1,18 @@
 import React, {Component} from 'react';
 import {Route, Switch,Redirect, Link, withRouter} from 'react-router-dom';
-import Users from './Machines';
+import Machines from './Machines';
     
 class Home extends Component {
     
     render() {
         return (
            <div>
-               <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+               <nav className="navbar navbar-expand-lg navbar-dark">
                    <Link className={"navbar-brand"} to={"/"}> Machine Gallery </Link>
                    <div className="collapse navbar-collapse" id="navbarText">
                        <ul className="navbar-nav mr-auto">
                            <li className="nav-item">
-                               
+                                <Link className={"nav-link"} to={"/machines"}> Machines</Link>
                            </li>
     
                            <li className="nav-item">
@@ -22,7 +22,8 @@ class Home extends Component {
                    </div>
                </nav>
                <Switch>
-                   <Redirect exact from="/" to="/users" />
+                   <Redirect exact from="/" to="/machines" />
+                   <Route path="/machines" component={Machines} />
                </Switch>
            </div>
         )
--- assets/js/components/Machines.js
+++ assets/js/components/Machines.js
@@ -1,7 +1,64 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+import React, {Component} from 'react';
+import axios from 'axios';
+    
+class Machines extends Component {
+    constructor() {
+        super();
+        this.state = { users: [], loading: true};
+    }
+    
+    componentDidMount() {
+        this.getMachines();
+    }
+    
+    getMachines() {
+       axios.get(`http://machine-gallery.local/machine/list`).then(machines => {
+           this.setState({ machines: machines.data, loading: false})
+       })
+    }
+    
+    render() {
+        const loading = this.state.loading;
+        return(
+            <div>
+                <section className="row-section">
+                    <div className="container">
+                        <div className="row">
+                            <h2 className="text-center"><span>Our machines</span>Find second hand machines at the best price and highest quality standards</h2>
+                        </div>
+                        {loading ? (
+                            <div className={'row text-center'}>
+                                <span className="fa fa-spin fa-spinner fa-4x"></span>
+                            </div>
+                        ) : (
+                            <div className={'row'}>
+                                { this.state.machines.map(machine =>
+                                    <div className="col-md-4 offset-md-1 row-block" key={machine.id}>
+                                        <ul id="sortable">
+                                            <li>
+                                                <div className="media">
+                                                    <div className="media-left align-self-center">
+                                                        <img className="rounded-circle"
+                                                             src={machine.images[0].url}/>
+                                                    </div>
+                                                    <div className="media-body">
+                                                        <h4>{machine.model}</h4>
+                                                        <p>{machine.brand} - {machine.manufacturer}</p>
+                                                    </div>
+                                                    <div className="media-right align-self-center">
+                                                        <a href="#" className="btn btn-default">Contact Now</a>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                )}
+                            </div>
+                        )}
+                    </div>
+                </section>
+            </div>
+        )
+    }
+}
+export default Machines;
