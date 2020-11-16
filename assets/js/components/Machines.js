e03374b91fa58e88d3ce6c8858efc2ed3f3e17d7
josep rigo <jrrr13@gmail.com>
2020-11-16 17:46:04 +0100

    Filter by brand, manufacturer and model

--- assets/js/components/Machines.js
+++ assets/js/components/Machines.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import {Route, Switch,Redirect, Link, withRouter} from 'react-router-dom';
 import axios from 'axios';
-    
+ 
 class Machines extends Component {
     constructor() {
         super();
-        this.state = { users: [], loading: true};
+        this.state = { machines: [], loading: true, search: ''};
     }
     
     componentDidMount() {
@@ -16,37 +17,64 @@ class Machines extends Component {
            this.setState({ machines: machines.data, loading: false})
        })
     }
+    updateSearch(event) {
+        this.setState({search:event.target.value});
+        
+    }
     
     render() {
         const loading = this.state.loading;
+        let filteredMachines = this.state.machines.filter(
+            (machine) => {
+                return machine.model.indexOf(this.state.search) !== -1 ||
+                machine.brand.indexOf(this.state.search) !== -1 ||
+                machine.manufacturer.indexOf(this.state.search) !== -1;
+            }  
+                );
         return(
             <div>
                 <section className="row-section">
                     <div className="container">
                         <div className="row">
                             <h2 className="text-center"><span>Our machines</span>Find second hand machines at the best price and highest quality standards</h2>
+                            
                         </div>
+                        <div className="row">
+                            <div className="col-md-6 ">
+                                <div className="form-group">
+                                    <input type="text"
+                                className="form-control"
+                            placeholder="filder by model, brand, manufacturer ..."
+                            value={this.state.search}
+                            onChange={this.updateSearch.bind(this)}
+                            />
+                                </div>
+                                
+                            </div>
+                            <div className="col-md-2">
+                                    <i className="fa fa-search"></i>
+                                 </div>
+                            
+                            </div>
                         {loading ? (
                             <div className={'row text-center'}>
                                 <span className="fa fa-spin fa-spinner fa-4x"></span>
                             </div>
                         ) : (
                             <div className={'row'}>
-                                { this.state.machines.map(machine =>
-                                    <div className="col-md-4 offset-md-1 row-block" key={machine.id}>
+                                { filteredMachines.map(machine =>
+                                    <div className="col-md-3" key={machine.id}>
+                                    <img className="" src={machine.images[0].url}/>
                                         <ul id="sortable">
                                             <li>
                                                 <div className="media">
-                                                    <div className="media-left align-self-center">
-                                                        <img className="rounded-circle"
-                                                             src={machine.images[0].url}/>
-                                                    </div>
+                                                   
                                                     <div className="media-body">
-                                                        <h4>{machine.model}</h4>
-                                                        <p>{machine.brand} - {machine.manufacturer}</p>
-                                                    </div>
-                                                    <div className="media-right align-self-center">
-                                                        <a href="#" className="btn btn-default">Contact Now</a>
+                                                        <h4>Model: {machine.model}</h4>
+                                                        <p> Brand: {machine.brand}</p>
+                                                        <p> Manufacturer: {machine.manufacturer}</p>
+                                                        <p> {machine.price} €</p>
+                                                       
                                                     </div>
                                                 </div>
                                             </li>
@@ -54,6 +82,7 @@ class Machines extends Component {
                                     </div>
                                 )}
                             </div>
+                                    
                         )}
                     </div>
                 </section>
